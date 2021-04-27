82f19d6b6ec787f5094f9afd100ef5bf90b84bd3
Adail Sales <65291508+AdailSales@users.noreply.github.com>
2021-04-27 06:42:27 -0300

    Add file via upload
    Foi adicionado o arquivo testarBuiltin.sh, script que testa se o parâmetro passado é ou não um comando builtin do Bash.

---
+++ testarBuiltin.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Testa se foi passado um argumento ...
+
+msg="É preciso informar o nome de um comando válido."
+
+[[ $# -ne 1 ]] && echo $msg && exit 1
+
+# Executa "help" para verificar se vai conseguir mostrar a ajuda do comando (sucesso)ou não (erro).
+
+help $1 &> /dev/null
+
+# Usar comando "test" parra avaliar a expressão ...
+
+[[ $? -eq 0 ]] && echo "É bultin." || echo "Não é builtin."
+
+exit 0 
